eeffe0fd8816d7de8a78127f6072c70b993f9574
James Strachan <james.strachan@gmail.com>
2010-01-20 14:26:15 +0000

    Initial attempt at JAXRS integration, using SSP for implicit views

--- ssp-core/src/main/scala/com/mh/serverpages/scala_/ScalaCompiler.scala
+++ ssp-core/src/main/scala/com/mh/serverpages/scala_/ScalaCompiler.scala
@@ -26,71 +26,73 @@ import scala.tools.nsc.Global
 import scala.tools.nsc.Settings
 import scala.tools.nsc.reporters.ConsoleReporter
 import java.net.URLClassLoader
+import javax.servlet.http.HttpServlet
 
 
 class ScalaCompiler extends Compiler
 {
-
-  override def compile( code: String, sourceDirectory: File, bytecodeDirectory: File, servletConfig: ServletConfig ): Unit = {
+  override def compile(code: String, sourceDirectory: File, bytecodeDirectory: File, servletConfig: ServletConfig): Unit = {
     // Prepare an object for collecting error messages from the compiler
     val messageCollector = new StringWriter
-    val messageCollectorWrapper = new PrintWriter( messageCollector )
+    val messageCollectorWrapper = new PrintWriter(messageCollector)
 
     // Initialize the compiler
-    val settings = generateSettings( bytecodeDirectory, buildClassPath( servletConfig ) )
-    val reporter = new ConsoleReporter( settings, Console.in, messageCollectorWrapper )
-    val compiler = new Global( settings, reporter )
+    val settings = generateSettings(bytecodeDirectory, buildClassPath(servletConfig))
+    val reporter = new ConsoleReporter(settings, Console.in, messageCollectorWrapper)
+    val compiler = new Global(settings, reporter)
 
     // Attempt compilation
-    ( new compiler.Run ).compile( findFiles( sourceDirectory ).map( _.toString ) )
+    (new compiler.Run).compile(findFiles(sourceDirectory).map(_.toString))
 
     // Bail out if compilation failed
-    if( reporter.hasErrors ) {
+    if (reporter.hasErrors) {
       reporter.printSummary
       messageCollectorWrapper.close
-      throw new ServerPageException( "Compilation failed:\n" + messageCollector.toString )
+      throw new ServerPageException("Compilation failed:\n" + messageCollector.toString)
     }
   }
 
 
-  private def error( message: String ): Unit = throw new ServerPageException( "Compilation failed:\n" + message )
+  private def error(message: String): Unit = throw new ServerPageException("Compilation failed:\n" + message)
 
 
-  private def buildClassPath( servletConfig: ServletConfig ): String = {
-    val classLoader = getClass.getClassLoader
-
-    // lets use the files on the URLClassLoader if we have one
-    val classLoaderList = classLoader match {
-      case u: URLClassLoader =>
-        List(u.getURLs.map{ _.getFile})
-
-      case _ => Nil
-    }
+  private def buildClassPath(servletConfig: ServletConfig): String = {
+    val containerList = classLoaderList(getClass) ::: classLoaderList(classOf[HttpServlet])
 
     // Always include WEB-INF/classes and all the JARs in WEB-INF/lib just in case
-    val classesDirectory = servletConfig.getServletContext.getRealPath( "/WEB-INF/classes" )
-    val libDirectory = servletConfig.getServletContext.getRealPath( "/WEB-INF/lib" )
-    val jars = findFiles( new File( libDirectory ) ).map { _.toString }
+    val classesDirectory = servletConfig.getServletContext.getRealPath("/WEB-INF/classes")
+    val libDirectory = servletConfig.getServletContext.getRealPath("/WEB-INF/lib")
+    val jars = findFiles(new File(libDirectory)).map {_.toString}
 
     // Allow adding a classpath prefix & suffix via web.xml
-    val prefix = servletConfig.getInitParameter( "compiler.classpath.prefix" ) match {
+    val prefix = servletConfig.getInitParameter("compiler.classpath.prefix") match {
       case null => Nil
       case path: String => List(path)
     }
-    val suffix = servletConfig.getInitParameter( "compiler.classpath.suffix" ) match {
+    val suffix = servletConfig.getInitParameter("compiler.classpath.suffix") match {
       case null => Nil
       case path: String => List(path)
     }
 
     // Put the pieces together
-    ( prefix ::: classLoaderList ::: classesDirectory :: jars ::: suffix ::: Nil ).mkString( ":" )
+
+    // TODO we should probably be smart enough to filter out duplicates here...
+    (prefix ::: containerList ::: classesDirectory :: jars ::: suffix ::: Nil).mkString(":")
   }
 
+  private def classLoaderList[T](aClass: Class[T]): List[String] = {
+    aClass.getClassLoader match {
+      case u: URLClassLoader =>
+        List.fromArray(u.getURLs).map {_.getFile}
 
-  private def generateSettings( bytecodeDirectory: File, classpath: String ): Settings = {
-    println ("using classpath: " + classpath)
-    
-    val settings = new Settings( error )
+      case _ => Nil
+    }
+  }
+
+  private def generateSettings(bytecodeDirectory: File, classpath: String): Settings = {
+    println("using classpath: " + classpath)
+
+    val settings = new Settings(error)
     settings.classpath.value = classpath
     settings.outdir.value = bytecodeDirectory.toString
     settings.deprecation.value = true
@@ -99,16 +101,16 @@ class ScalaCompiler extends Compiler
   }
 
 
-  private def findFiles( root: File ): List[File] = {
-    if( root.isFile )
-      List( root )
+  private def findFiles(root: File): List[File] = {
+    if (root.isFile)
+      List(root)
     else
-      makeList( root.listFiles ).flatMap { f => findFiles( f ) }
+      makeList(root.listFiles).flatMap {f => findFiles(f)}
   }
 
 
-  private def makeList( a: Array[File] ): List[File] = {
-    if( a == null )
+  private def makeList(a: Array[File]): List[File] = {
+    if (a == null)
       Nil
     else
       a.toList
