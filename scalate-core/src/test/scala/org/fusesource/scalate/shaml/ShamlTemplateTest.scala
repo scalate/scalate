1951b11b045ba370c8967ca140bec3eb9028e94c
Hiram Chirino <hiram@hiramchirino.com>
2010-02-02 08:12:40 -0500

    Adding more shaml tests

--- scalate-core/src/test/scala/org/fusesource/scalate/shaml/ShamlTemplateTest.scala
+++ scalate-core/src/test/scala/org/fusesource/scalate/shaml/ShamlTemplateTest.scala
@@ -30,23 +30,325 @@ import util.XmlEscape
 @RunWith(classOf[JUnitRunner])
 class ShamlTemplateTest extends FunSuite {
 
-  test("simple") {
+  test("'%tag' renders a tag") {
+    expect(
+"""
+<html></html>
+"""
+    ) {render(
+"""
+%html
+"""
+    )}
+  }
+
+  test("'%tag#i1' renders a tag with an id") {
+    expect(
+"""
+<html id="i1"></html>
+"""
+    ) {render(
+"""
+%html#i1
+"""
+    )}
+  }
+
+  test("'%tag#i1#i2' last id specified wins") {
+    expect(
+"""
+<html id="i2"></html>
+"""
+    ) {render(
+"""
+%html#i1#i2
+"""
+    )}
+  }
+
+  test("'%tag.c1' renders a tag with a class") {
+    expect(
+"""
+<html class="c1"></html>
+"""
+    ) {render(
+"""
+%html.c1
+"""
+    )}
+  }
+
+  test("'%tag.c1.c2' renders a tag with multiple classes") {
+    expect(
+"""
+<html class="c1 c2"></html>
+"""
+    ) {render(
+"""
+%html.c1.c2
+"""
+    )}
+  }
+
+  test("'.c1' if tag name is omitted, it defaults to div") {
+    expect(
+"""
+<div class="c1"></div>
+"""
+    ) {render(
+"""
+.c1
+"""
+    )}
+  }
+  
+  ignore("'%tag/' renders a closed tag") {
+    expect(
+"""
+<html/>
+"""
+    ) {render(
+"""
+%html/
+"""
+    )}
+  }
+
+  ignore("'%tag{:name -> \"value\"}' tag attributes can be specified using a ruby hash syntax") {
+    expect(
+"""
+<html k1="v1" k2="v2"></html>
+"""
+    ) {render(
+"""
+%html{:k1->"v1", "k2"->"v2"}
+"""
+    )}
+  }
+
+  ignore("'%tag{:name -> \"value\"}' tag attributes using hash syntax can span multiple lines after the comma") {
+    expect(
+"""
+<html>
+  <body k1="v1" k2="v2"></body>
+</html>
+
+"""
+    ) {render(
+"""
+%html
+  %body{:k1->"v1",
+"k2"->"v2"}
+"""
+    )}
+  }
+
+  test("'%tag(name=\"value\")' tag attributes can be specified html attribute syntax") {
+    expect(
+"""
+<html k1="v1" k2="v2"></html>
+"""
+    ) {render(
+"""
+%html(k1="v1" k2="v2")
+"""
+    )}
+  }
+
+  test("Plain text is rendered as plain text") {
+    expect(
+"""
+this is
+plain text
+"""
+    ) {render(
+"""
+this is
+plain text
+"""
+    )}
+  }
+
+  test("Plain text can be nested in a tag") {
+    expect(
+"""
+<html>
+  this is
+  plain text
+</html>
+"""
+    ) {render(
+"""
+%html
+  this is
+  plain text
+"""
+    )}
+  }
+
+  test("'%tag text' render start tag, text, and end tag on same line") {
+    expect(
+"""
+<html>test</html>
+"""
+    ) {render(
+"""
+%html test
+"""
+    )}
+  }
+
+  test("nested tags are rendered indented") {
     expect(
 """
 <html>
   <body>
-    <p>test</p>
+    test
   </body>
 </html>
 """
-      ) {render(
+    ) {render(
 """
 %html
   %body
-    %p test
+    test
 """
-      )}
+    )}
   }
+
+  test("'= expression' renders a dynamic expression") {
+    expect(
+"""
+<html>
+  <body>
+    10
+  </body>
+</html>
+"""
+    ) {render(
+"""
+%html
+  %body
+    = 5 + 5
+"""
+    )}
+  }
+
+  test("'= var' expressions can acess implicitly bound variables") {
+    expect(
+"""
+<html>
+  <body>
+    Hiram
+  </body>
+</html>
+"""
+    ) {render(
+"""
+%html
+  %body
+    = context.name
+"""
+    )}
+  }
+
+  test("'= var' expressions can access imported variables") {
+    expect(
+"""
+<html>
+  <body>
+    Hiram
+  </body>
+</html>
+"""
+    ) {render(
+"""
+%html
+  %body
+    = name
+"""
+    )}
+  }
+
+  test("'%tag= expression' render start tag, exression result, and end tag on same line") {
+    expect(
+"""
+<html>
+  <body>Hiram</body>
+</html>
+"""
+    ) {render(
+"""
+%html
+  %body= name
+"""
+    )}
+  }
+
+
+  test("'/ text' renders an html comment") {
+    expect(
+"""
+<html>
+  <body>
+    <!--Test-->
+  </body>
+</html>
+"""
+    ) {render(
+"""
+%html
+  %body
+    /Test
+"""
+    )}
+  }
+
+  ignore("'/' can html comment a whole block of shaml") {
+    expect(
+"""
+<html>
+  <!--
+    <body>
+      Test
+    </body>
+  -->
+</html>
+"""
+    ) {render(
+"""
+%html
+  /
+    %body
+      Test
+"""
+    )}
+  }
+
+  ignore("'/[condition]' creates a conditional comment") {
+    expect(
+"""
+<html>
+  <!--[if IE]>
+    <body>
+      Test
+    </body>
+  <![endif]-->
+</html>
+"""
+    ) {render(
+"""
+%html
+  /[if IE]
+    %body
+      Test
+"""
+    )}
+  }
+
+
+
+
 
   def render(content:String): String = {
     var engine = new TemplateEngine
@@ -65,13 +367,12 @@ class ShamlTemplateTest extends FunSuite {
 
 }
 
-/**
- * A default template context for use outside of servlet environments
- */
 case class Context() extends RenderCollector() {
 
   val buffer = new StringWriter()
   val out = new PrintWriter(buffer)
+
+  val name = "Hiram"
 
   def <<(value: Any): Unit = {
     out.print(value.toString)
