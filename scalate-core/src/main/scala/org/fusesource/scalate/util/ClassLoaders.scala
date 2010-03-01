c097aab79518a4b7d58a12137094f5ca8d6a610a
James Strachan <james.strachan@gmail.com>
2010-03-01 08:44:08 +0000

    attempt at fix for windows

--- scalate-core/src/main/scala/org/fusesource/scalate/util/ClassLoaders.scala
+++ scalate-core/src/main/scala/org/fusesource/scalate/util/ClassLoaders.scala
@@ -20,7 +20,7 @@ object ClassLoaders extends Logging {
     classLoader match {
       case cl: URLClassLoader =>
         cl.getURLs.toList.map {
-          u => val n = new File(u.getFile).getCanonicalPath
+          u => val n = new File(u.toExternalForm).getCanonicalPath
           if (n.contains(' ')) {"\"" + n + "\""} else {n}
         }
 
