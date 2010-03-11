c05b24ecaea7a077333580f17dbda94f83c867d4
Spiros Tzavellas <spiros@tzavellas.com>
2010-03-11 11:24:18 +0200

    More code cleanups.

--- scalate-core/src/main/scala/org/fusesource/scalate/jersey/SSPTemplateProcessor.scala
+++ scalate-core/src/main/scala/org/fusesource/scalate/jersey/SSPTemplateProcessor.scala
@@ -1,8 +1,8 @@
 package org.fusesource.scalate.jersey
 
-import java.io.{OutputStream}
+import java.io.OutputStream
 import java.net.MalformedURLException
-import javax.servlet.{ServletContext}
+import javax.servlet.ServletContext
 import com.sun.jersey.api.view.Viewable
 import com.sun.jersey.spi.template.ViewProcessor
 import com.sun.jersey.server.impl.container.servlet.RequestDispatcherWrapper
@@ -16,6 +16,8 @@ import javax.ws.rs.core.Context
  * @version $Revision : 1.1 $
  */
 class SSPTemplateProcessor(@Context resourceConfig: ResourceConfig) extends ViewProcessor[String] with Logging {
+  import SSPTemplateProcessor._
+  
   @Context
   var servletContext: ServletContext = _
   @Context
@@ -26,14 +28,8 @@ class SSPTemplateProcessor(@Context resourceConfig: ResourceConfig) extends View
   var response: HttpServletResponse = _
 
   val basePath = resourceConfig.getProperties().get("org.fusesource.config.property.SSPTemplatesBasePath") match {
-    case path: String =>
-      if (path(0) == '/') {
-        path
-      }
-      else {
-        "/" + path
-      }
-    case _ => ""
+    case path: String => if (path(0) == '/') path else "/" + path
+    case _            => ""
   }
 
 
@@ -45,36 +41,36 @@ class SSPTemplateProcessor(@Context resourceConfig: ResourceConfig) extends View
 
     try {
       val path = if (basePath.length > 0) basePath + requestPath else requestPath
-
-      SSPTemplateProcessor.suffixes.map {path + _}.
-        find {servletContext.getResource(_) ne null} getOrElse null
+      templateSuffixes.map  { path + _ }
+                      .find { servletContext.getResource(_) ne null }
+                      .getOrElse(null)
     } catch {
       case e: MalformedURLException =>
-        // TODO log
+        warning("Tried to load template using Malformed URL. " + e.getMessage)
         null
     }
   }
 
   def writeTo(resolvedPath: String, viewable: Viewable, out: OutputStream): Unit = {
     // Ensure headers are committed
-    out.flush();
+    out.flush()
 
-    val dispatcher = servletContext.getRequestDispatcher(resolvedPath);
+    val dispatcher = servletContext.getRequestDispatcher(resolvedPath)
     if (dispatcher == null) {
-      throw new ContainerException("No request dispatcher for: " + resolvedPath);
+      throw new ContainerException("No request dispatcher for: " + resolvedPath)
     }
 
-    val wrapper = new RequestDispatcherWrapper(dispatcher, basePath, hc, viewable);
     try {
-      wrapper.forward(request, response);
-      //wrapper.forward(requestInvoker.get(), responseInvoker.get());
+      val wrapper = new RequestDispatcherWrapper(dispatcher, basePath, hc, viewable)
+      wrapper.forward(request, response)
+      //wrapper.forward(requestInvoker.get(), responseInvoker.get())
     } catch {
-      case e: Exception => throw new ContainerException(e);
+      case e: Exception => throw new ContainerException(e)
     }
   }
 
 }
 
 object SSPTemplateProcessor {
-  val suffixes = List("", ".ssp", ".scaml")
+  private val templateSuffixes = List("", ".ssp", ".scaml")
 }
--- scalate-core/src/main/scala/org/fusesource/scalate/layout/LayoutStrategy.scala
+++ scalate-core/src/main/scala/org/fusesource/scalate/layout/LayoutStrategy.scala
@@ -3,13 +3,25 @@ package org.fusesource.scalate.layout
 import org.fusesource.scalate.{RenderContext, Template}
 
 /**
+ * A strategy for loading and applying layout templates.
+ * 
  * @version $Revision : 1.1 $
  */
-
 trait LayoutStrategy {
+  
+  /**
+   * Render the specified template by decorating it with another "layout template".
+   * 
+   * @param template the template to render
+   * @param context  the context that will be used for rendering the template
+   */
   def layout(template: Template, context: RenderContext): Unit
 }
 
+/**
+ * A <code>LayoutStrategy</code> that renders the given template without
+ * using any layout. 
+ */
 class NullLayoutStrategy extends LayoutStrategy {
   def layout(template: Template, context: RenderContext) = template.render(context)
 }
