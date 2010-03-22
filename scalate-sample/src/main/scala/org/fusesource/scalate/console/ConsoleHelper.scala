4f1d900f6efeb04c799df7e4561eab07472c4828
James Strachan <james.strachan@gmail.com>
2010-03-22 17:02:57 +0000

    fixed search/replace error

--- scalate-sample/src/main/scala/org/fusesource/scalate/console/ConsoleHelper.scala
+++ scalate-sample/src/main/scala/org/fusesource/scalate/console/ConsoleHelper.scala
@@ -264,7 +264,7 @@ class ConsoleHelper(context: ServletRenderContext) extends Logging {
   // Error Handling helper methods
   //-------------------------------------------------------------------------
   def exception = attributes("javax.servlet.error.exception")
-  def oldError = attributeOrElse("javax.servlet.error.oldError", "")
+  def oldError = attributeOrElse("javax.servlet.error.olderror", "")
   def errorRequestUri = attributeOrElse("javax.servlet.error.request_uri", "")
   def errorCode = attributeOrElse("javax.servlet.error.status_code", 500)
 
