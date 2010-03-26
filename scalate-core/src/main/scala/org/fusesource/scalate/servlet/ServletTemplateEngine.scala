f6a0bb7f7f06edaf8c38c614b6ed22e7de77ff19
James Strachan <james.strachan@gmail.com>
2010-03-26 15:26:44 +0000

    use the scalate.workdir system property by default if its set - also default it to go into the target/scalateWorkDir by default in jetty-run

--- scalate-core/src/main/scala/org/fusesource/scalate/servlet/ServletTemplateEngine.scala
+++ scalate-core/src/main/scala/org/fusesource/scalate/servlet/ServletTemplateEngine.scala
@@ -94,8 +94,9 @@ class ServletTemplateEngine(var config: ServletConfig) extends TemplateEngine {
   }
 
   def useWebInfWorkingDirectory = {
+    val customWorkDir = System.getProperty("scalate.workingdir", "")
     val property = System.getProperty("scalate.temp.workingdir", "")
     println("using scalate.temp.workingdir: " + property)
-    property.toLowerCase != "true"
+    property.toLowerCase != "true" && customWorkDir.length <= 0
   }
 }
