9fbbed54ed755ccc9669b27ea35467c158a0ee2b
Hiram Chirino <hiram@hiramchirino.com>
2010-04-26 13:37:18 -0400

    Fixes #76 : Remove scalate.temp.workingdir system property option for consistency

--- scalate-core/src/main/scala/org/fusesource/scalate/servlet/ServletTemplateEngine.scala
+++ scalate-core/src/main/scala/org/fusesource/scalate/servlet/ServletTemplateEngine.scala
@@ -59,13 +59,17 @@ object ServletTemplateEngine {
  */
 class ServletTemplateEngine(var config: ServletConfig) extends TemplateEngine {
   bindings = List(Binding("context", classOf[ServletRenderContext].getName, true, isImplicit = true))
-  
-  if (useWebInfWorkingDirectory) {
+
+  // If the scalate.workingdir is not set, then just configure the working
+  // directory under WEB_INF/_scalate
+  if ( System.getProperty("scalate.workingdir", "").isEmpty ) {
+
     val path = config.getServletContext.getRealPath("WEB-INF")
     if (path != null) {
       workingDirectory = new File(path, "_scalate")
       workingDirectory.mkdirs
     }
+
   }
   classpath = buildClassPath
   resourceLoader = new ServletResourceLoader(config.getServletContext)
@@ -94,10 +98,4 @@ class ServletTemplateEngine(var config: ServletConfig) extends TemplateEngine {
     builder.classPath
   }
 
-  def useWebInfWorkingDirectory = {
-    val customWorkDir = System.getProperty("scalate.workingdir", "")
-    val property = System.getProperty("scalate.temp.workingdir", "")
-    println("using scalate.temp.workingdir: " + property)
-    property.toLowerCase != "true" && customWorkDir.length <= 0
-  }
 }
