bf476bce2f57cb5fa493145b78a86e013eb76d8b
Hiram Chirino <hiram@hiramchirino.com>
2010-06-30 13:21:36 -0400

    Added a new ScalateTemplateProvider which allows you to render DTOs from jersey controls via Scalate template.

---
+++ scalate-core/src/main/scala/org/fusesource/scalate/jersey/ScalateTemplateProvider.scala
@@ -0,0 +1,111 @@
+package org.fusesource.scalate.jersey
+
+import java.io.OutputStream
+import java.net.MalformedURLException
+import javax.servlet.ServletContext
+import com.sun.jersey.api.view.Viewable
+import com.sun.jersey.spi.template.ViewProcessor
+import com.sun.jersey.server.impl.container.servlet.RequestDispatcherWrapper
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import com.sun.jersey.api.core.{HttpContext, ResourceConfig}
+import org.fusesource.scalate.util.Logging
+import com.sun.jersey.api.container.ContainerException
+import org.fusesource.scalate.TemplateEngine
+import java.lang.{String, Class}
+import javax.ws.rs.core.{MultivaluedMap, MediaType, Context}
+import java.lang.annotation.Annotation
+import java.lang.reflect.Type
+import org.fusesource.scalate.servlet.{ServletTemplateEngine, ServletHelper, TemplateEngineServlet}
+import org.fusesource.scalate.support.ResourceLoader
+import javax.ws.rs.ext.{Provider, MessageBodyWriter, MessageBodyReader}
+import javax.ws.rs.Produces
+
+/**
+ * A template provider for <a href="https://jersey.dev.java.net/">Jersey</a> using Scalate templates
+ * to produce HTML of an object.
+ *
+ * @version $Revision : 1.1 $
+ */
+@Provider
+class ScalateTemplateProvider(@Context resourceConfig: ResourceConfig) extends MessageBodyWriter[AnyRef] with Logging {
+
+  @Context
+  var servletContext: ServletContext = _
+  @Context
+  var request: HttpServletRequest = _
+  @Context
+  var response: HttpServletResponse = _
+
+  var templateDirectories = List("/WEB-INF", "")
+
+  def resolve(loader:ResourceLoader, argType:Class[_]):String = {
+    val argBase = argType.getName.replace('.','/')
+
+    templateDirectories.foreach { dir =>
+      TemplateEngine.templateTypes.foreach { types =>
+        val path = dir + "/" + argBase + "." + types
+        if( loader.exists(path) ) {
+          return path
+        }
+      }
+    }
+    null
+  }
+
+  def getSize(arg: AnyRef, argType : Class[_], genericType: Type, annotations: Array[Annotation], mediaType: MediaType) = -1L
+
+  def isWriteable(argType: Class[_], genericType: Type, annotations: Array[Annotation], mediaType: MediaType) = {
+    if(mediaType.getType == "text" && mediaType.getSubtype == "html") {
+      val servlet = TemplateEngineServlet()
+      val path = resolve(servlet.templateEngine.resourceLoader, argType)
+      path != null
+    } else {
+      false
+    }
+  }
+
+  def writeTo(arg:AnyRef, argType:Class[_], genericType: Type, annotations: Array[Annotation], media: MediaType, headers: MultivaluedMap[String, AnyRef], out: OutputStream) = {
+    // Ensure headers are committed
+    out.flush()
+
+    val servlet = TemplateEngineServlet()
+    val path = resolve(servlet.templateEngine.resourceLoader, argType)
+    path != null
+
+    request.setAttribute("it", arg)
+
+    try {
+      servlet.render(path, request, response)
+    } catch {
+      case e: Exception =>
+        // lets forward to the error handler
+        var notFound = true
+        for (uri <- ServletHelper.errorUris() if notFound) {
+          if (servletContext.getResource(uri) != null) {
+
+            // we need to expose all the errors property here...
+            request.setAttribute("javax.servlet.error.exception", e)
+            request.setAttribute("javax.servlet.error.exception_type", e.getClass)
+            request.setAttribute("javax.servlet.error.message", e.getMessage)
+            request.setAttribute("javax.servlet.error.request_uri", request.getRequestURI)
+            request.setAttribute("javax.servlet.error.servlet_name", request.getServerName)
+
+            // TODO how to get the status code???
+            val status = 500
+            request.setAttribute("javax.servlet.error.status_code", status)
+
+            request.setAttribute("it", e)
+            servlet.render(uri, request, response)
+            notFound = false
+          }
+        }
+        if (notFound) {
+          throw new ContainerException(e)
+        }
+    }
+  }
+
+
+
+
+}
