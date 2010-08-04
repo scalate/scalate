967160a021f421a80850a43f48a57d27c5cf3e2e
James Strachan <james.strachan@gmail.com>
2010-08-04 10:48:11 +0100

    add helper so when we run "mvn compile scala:console" we have a populated REPL where we can tinker with the convert tool

---
+++ scalate-jsp-converter/src/test/scala/ConsoleImports.scala
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2009-2010 the original author or authors.
+ * See the notice.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// console script to auto import some useful stuff when we start the console
+
+import java.io._
+import org.fusesource.scalate.converter._
+
+val tool = new JspConvert
+
