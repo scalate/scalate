f358994881add7d0fd143d2c3cee5f8b42a54a76
Hiram Chirino <hiram@hiramchirino.com>
2010-08-06 22:56:59 -0400

    Fixes failing test due recent change which only enable stack traces when in dev mode.

--- scalate-sample/src/test/scala/org/fusesource/scalate/sample/SampleTest.scala
+++ scalate-sample/src/test/scala/org/fusesource/scalate/sample/SampleTest.scala
@@ -30,6 +30,11 @@ import _root_.org.fusesource.scalate.test._
 @RunWith(classOf[JUnitRunner])
 class SampleTest extends FunSuite with WebServerMixin with WebDriverMixin {
 
+  override protected def beforeAll(configMap: Map[String, Any]) = {
+    System.setProperty("scalate.mode", "development")
+    super.beforeAll(configMap)
+  }
+
   testPageContains("ssp/renderObject.ssp", "Strachan")
 
   test("home page") {
