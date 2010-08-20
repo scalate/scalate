d5ac92fd4885cd815a911e8506d6f4c2fae43646
James Strachan <james.strachan@gmail.com>
2010-08-20 17:06:39 +0100

    added a first cut at a filter implementation for confluence wiki markup using wikitext from mylyn plus a little sample local-wiki web app (for generating static web content on OSS projects)

---
+++ scalate-wikitext/src/test/scala/org/fusesource/scalate/wikitext/ConfluenceTest.scala
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2009-2010 the original author or authors.
+ * See the notice.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fusesource.scalate.wikitext
+
+import org.fusesource.scalate.test.FunSuiteSupport
+
+class ConfluenceTest extends FunSuiteSupport {
+  val filter = new ConfluenceFilter
+
+  test("parse confluence wiki") {
+    assertFilter(
+"""h1. Title
+Hello
+* one
+* two
+""",
+    
+"""<h1 id="Title">Title</h1><p>Hello</p><ul><li>one</li><li>two</li></ul>""")
+  }
+
+  protected def assertFilter(source: String, expected: String): Unit = {
+    println("Converting: " + source)
+    val actual = filter.filter(source)
+    println("Created: " + actual)
+    expect(expected) {actual}
+  }
+}
