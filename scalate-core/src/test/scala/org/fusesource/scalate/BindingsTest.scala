ae15f43a8a2307cc2867760d7de6d04b3aae0468
James Strachan <james.strachan@gmail.com>
2010-08-26 11:06:34 +0100

    added a test case to ensure Esfand's issue is fixed. see the discussion: http://groups.google.com/group/scalate/browse_thread/thread/1f135f9e2b456d15

--- scalate-core/src/test/scala/org/fusesource/scalate/BindingsTest.scala
+++ scalate-core/src/test/scala/org/fusesource/scalate/BindingsTest.scala
@@ -4,12 +4,24 @@ class BindingsTest extends TemplateTestSupport {
   test("can use a template with default binding expression") {
     val responseClassName = classOf[DummyResponse].getName
     engine.bindings = List(
-    Binding("context", classOf[DefaultRenderContext].getName, true, isImplicit = true),
-    Binding("response", responseClassName, defaultValue = Some("new " + responseClassName + "()")))
+      Binding("context", classOf[DefaultRenderContext].getName, true, isImplicit = true),
+      Binding("response", responseClassName, defaultValue = Some("new " + responseClassName + "()")))
 
-    val text = engine.layout(TemplateSource.fromText("foo.ssp","hello ${response}"))
+    val text = engine.layout(TemplateSource.fromText("foo.ssp", "hello ${response}"))
 
-    println("Got: " + text)
+    info("Got: " + text)
+  }
+
+  test("Int binding with a default value") {
+    engine.bindings = List(Binding("year", "Int", false, Some("1970")))
+
+    val text1 = engine.layout(TemplateSource.fromText("foo2.ssp", "${year.toString}'s is the hippies era"))
+    info("Got: " + text1)
+    expect("1970's is the hippies era") {text1.trim}
+
+    val text2 = engine.layout(TemplateSource.fromText("foo3.ssp", "${year.toString}'s is the hippies era"), Map("year" -> 1950))
+    info("Got: " + text2)
+    expect("1950's is the hippies era") {text2.trim}
   }
 }
 
