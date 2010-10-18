2b4ef4eb1e2bb7e7cb9eaf9c663d48317eac99bf
James Strachan <james.strachan@gmail.com>
2010-10-18 20:16:11 +0100

    improved the test case a little

--- scalate-core/src/test/scala/org/fusesource/scalate/ssp/AndyWEscapeTest.scala
+++ scalate-core/src/test/scala/org/fusesource/scalate/ssp/AndyWEscapeTest.scala
@@ -4,10 +4,10 @@ package ssp
 class AndyWEscapeTest extends TemplateTestSupport {
 
   test("escape text") {
-    showOutput = true
-
     assertOutputContains(fromUri("org/fusesource/scalate/ssp/escape.ssp"),
-     """<div id="a">foo=&quot;bar baz&quot; &lt;this&gt;</div>"""
+     """<div id="a">foo=&quot;bar baz&quot; &lt;this&gt;</div>""" ,
+     """<div id="b">foo=&quot;bar baz&quot; &lt;this&gt;</div>""" ,
+     """<div id="c">foo=&quot;bar baz&quot; &lt;this&gt;</div>"""
     )
   }
 
