06344fdd984f3874f5e1c55b8a5df096a41abb56
James Strachan <james.strachan@gmail.com>
2010-12-14 17:12:47 +0000

    fixed up the documentation on JOG based on recent feedback

--- scalate-website/src/main/scala/Website.scala
+++ scalate-website/src/main/scala/Website.scala
@@ -36,7 +36,7 @@ object Website {
   val project_wiki_url= "http://wiki.github.com/scalate/scalate/"
   val project_logo= "/images/project-logo.png"
   val project_version= "1.3.2"
-  val project_snapshot_version= "1.4.0.SNAPSHOT"
+  val project_snapshot_version= "1.4.0-SNAPSHOT"
 
   val github_page= "http://github.com/scalate/scalate"
   val git_user_url= "git://github.com/scalate/scalate.git"
