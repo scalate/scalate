325f72a5d526f8ea05afb3ce379c8aa0c51a4d2d
Hiram Chirino <hiram@hiramchirino.com>
2011-02-08 14:11:47 -0500

    Better

--- samples/scalate-sample-sitegen/project/build/SampleSiteGenProject.scala
+++ samples/scalate-sample-sitegen/project/build/SampleSiteGenProject.scala
@@ -7,6 +7,17 @@ class SampleSiteGenProject(info: ProjectInfo) extends DefaultWebProject(info) wi
 
   val fuseSourceSnapshots = "FuseSource Snapshot Repository" at "http://repo.fusesource.com/nexus/content/repositories/snapshots"
 
+  val scalate = "org.fusesource.scalate" % "scalate-core" % "1.4.0-SNAPSHOT"
+  val servlet = "javax.servlet" % "servlet-api" % "2.5" 
+  val slf4j = "org.slf4j" % "slf4j-nop" % "1.6.1"
+
   val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.RC0" % "test"
-  val scalate = "org.fusesource.scalate" % "scalate-core" % "1.4.0-SNAPSHOT" % "compile"
+
+  //
+  // We don't use a standard directory layout
+  //
+  override def mainScalaSourcePath = "ext"
+  override def mainResourcesPath = "resources"
+  override def webappPath = "src"
+    
 }
--- samples/scalate-sample-sitegen/project/plugins/Plugins.scala
+++ samples/scalate-sample-sitegen/project/plugins/Plugins.scala
@@ -4,6 +4,7 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info)
 {
-  val scalatePlugin = "org.fusesource.scalate" % "sbt-scalate-plugin" % "1.4.0-SNAPSHOT"
   val fuseSourceSnapshots = "FuseSource Snapshot Repository" at "http://repo.fusesource.com/nexus/content/repositories/snapshots"
+
+  val scalatePlugin = "org.fusesource.scalate" % "sbt-scalate-plugin" % "1.4.0-SNAPSHOT"
 }
