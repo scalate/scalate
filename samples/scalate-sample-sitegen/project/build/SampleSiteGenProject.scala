723939a46df248e587ef999f68fefc60366d0035
Hiram Chirino <hiram@hiramchirino.com>
2011-02-08 13:37:03 -0500

    getting further with these sbt changes.

--- samples/scalate-sample-sitegen/project/build/SampleSiteGenProject.scala
+++ samples/scalate-sample-sitegen/project/build/SampleSiteGenProject.scala
@@ -1,7 +1,12 @@
 package org.fusesource.scalate.samples
 
-import sbt._
+import _root_.sbt._
+import org.fusesource.scalate.sbt._
 
 class SampleSiteGenProject(info: ProjectInfo) extends DefaultWebProject(info) with SiteGenProject {
+
+  val fuseSourceSnapshots = "FuseSource Snapshot Repository" at "http://repo.fusesource.com/nexus/content/repositories/snapshots"
+
+  val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.RC0" % "test"
   val scalate = "org.fusesource.scalate" % "scalate-core" % "1.4.0-SNAPSHOT" % "compile"
 }
