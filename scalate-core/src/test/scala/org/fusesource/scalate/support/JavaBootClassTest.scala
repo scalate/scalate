2ee39e7d1aa48d58302aefbd2a7c0b6ebf42a8d9
James Strachan <james.strachan@gmail.com>
2011-03-14 13:10:18 +0000

    explicitly reset the test counter just in case

--- scalate-core/src/test/scala/org/fusesource/scalate/support/JavaBootClassTest.scala
+++ scalate-core/src/test/scala/org/fusesource/scalate/support/JavaBootClassTest.scala
@@ -3,6 +3,8 @@ package support
 
 class JavaBootClassTest extends TemplateTestSupport {
   test("Java Boot class") {
+    MyJavaBoot.runCount = 0
+
     Boots.invokeBoot(classOf[MyJavaBoot], List(engine))
 
     expect(1) { MyJavaBoot.runCount }
