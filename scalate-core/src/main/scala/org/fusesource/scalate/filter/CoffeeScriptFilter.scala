3d1c6166ce7f08f8c8dce225c6f5d21fa0d7e333
Hiram Chirino <hiram@hiramchirino.com>
2011-04-28 13:14:20 -0400

    Fixes #239 : Adding a cofeescript filter.

---
+++ scalate-core/src/main/scala/org/fusesource/scalate/filter/CoffeeScriptFilter.scala
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2009-2011 the original author or authors.
+ * See the notice.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fusesource.scalate
+package filter
+
+import org.fusesource.scalate.support.RenderHelper
+
+/**
+ * Surrounds the filtered text with &lt;script&gt; and CDATA tags.
+ * 
+ * <p>Useful for including inline Javascript.</p>
+ *
+ * @author <a href="http://hiramchirino.com">Hiram Chirino</a>
+ */
+object CoffeeScriptFilter extends Filter {
+
+  def filter(context: RenderContext, content: String) = {
+
+    // TODO: 'if( !context.engine.isDevelopmentMode )' then
+    // we should try to compile the coffescript on the server
+    // side and cache it.
+
+    context.attributes("REQUIRES_COFFEE_SCRIPT_JS") = "true"
+    """<script type='text/coffeescript'>
+       |  #<![CDATA[
+       |    """.stripMargin+RenderHelper.indent("    ", content)+"""
+       |  #]]>
+       |</script>""".stripMargin
+  }
+  
+}
