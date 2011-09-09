978df584e66f14a033e22a10b04d12cc8f0353a0
Dag Liodden <dag@liodden.no>
2011-09-08 23:20:28 -0400

    Coffeescript compilation support for coffeescript filter

--- scalate-core/src/main/scala/org/fusesource/scalate/filter/CoffeeScriptFilter.scala
+++ scalate-core/src/main/scala/org/fusesource/scalate/filter/CoffeeScriptFilter.scala
@@ -19,6 +19,8 @@ package org.fusesource.scalate
 package filter
 
 import org.fusesource.scalate.support.RenderHelper
+import org.mozilla.javascript._
+import java.io.InputStreamReader
 
 /**
  * Surrounds the filtered text with &lt;script&gt; and CDATA tags.
@@ -31,16 +33,71 @@ object CoffeeScriptFilter extends Filter {
 
   def filter(context: RenderContext, content: String) = {
 
-    // TODO: 'if( !context.engine.isDevelopmentMode )' then
-    // we should try to compile the coffescript on the server
-    // side and cache it.
-
-    context.attributes("REQUIRES_COFFEE_SCRIPT_JS") = "true"
-    """<script type='text/coffeescript'>
-       |  #<![CDATA[
-       |    """.stripMargin+RenderHelper.indent("    ", content)+"""
-       |  #]]>
-       |</script>""".stripMargin
+    if( !context.engine.isDevelopmentMode )
+      Compiler.compile(content, Some(context.currentTemplate)).fold({ error =>
+        throw new CompilerException(error.message, Nil)
+      }, { coffee =>
+        """<script type='text/javascript'>
+           |  #<![CDATA[
+           |    """.stripMargin+RenderHelper.indent("    ", coffee)+"""
+           |  #]]>
+           |</script>""".stripMargin
+      })
+    else {
+      context.attributes("REQUIRES_COFFEE_SCRIPT_JS") = "true"
+      """<script type='text/coffeescript'>
+         |  #<![CDATA[
+         |    """.stripMargin+RenderHelper.indent("    ", content)+"""
+         |  #]]>
+         |</script>""".stripMargin
+    }
   }
-  
+
+  /**
+   * A Scala / Rhino Coffeescript compiler.
+   */
+  object Compiler {
+
+    /**
+     * Compiles a string of Coffeescript code to Javascript.
+     *
+     * @param code the Coffeescript code
+     * @param sourceName a descriptive name for the code unit under compilation (e.g a filename)
+     * @param bare if true, no function wrapper will be generated
+     * @return the compiled Javascript code
+     */
+    def compile(code: String, sourceName: Option[String] = None, bare : Boolean = false)
+      : Either[CompilationError, String] = withContext { ctx =>
+      val scope = ctx.initStandardObjects()
+      ctx.evaluateReader(
+        scope,
+        new InputStreamReader(getClass.getResourceAsStream("coffee-script.js"), "UTF-8"),
+        "coffee-script.js", 1, null
+      )
+
+      val coffee = scope.get("CoffeeScript", scope).asInstanceOf[NativeObject]
+      val compileFunc = coffee.get("compile", scope).asInstanceOf[Function]
+      val opts = ctx.evaluateString(scope, "({bare: %b});".format(bare), null, 1, null)
+
+      try {
+        Right(compileFunc.call(ctx, scope, coffee, Array(code, opts)).asInstanceOf[String])
+      } catch {
+        case e : JavaScriptException =>
+          Left(CompilationError(sourceName, e.getValue.toString))
+      }
+    }
+
+    def withContext[T](f: Context => T): T = {
+      val ctx = Context.enter()
+      try {
+        ctx.setOptimizationLevel(-1) // Do not compile to byte code (max 64kb methods)
+        f(ctx)
+      } finally {
+        Context.exit()
+      }
+    }
+  }
+
+  case class CompilationError(sourceName: Option[String], message: String)
 }
+
---
+++ scalate-core/src/test/scala/org/fusesource/scalate/filter/CoffeeScriptFilterTest.scala
@@ -0,0 +1,18 @@
+package org.fusesource.scalate.filter
+
+import org.fusesource.scalate.TemplateTestSupport
+
+class CoffeeScriptFilterTest extends TemplateTestSupport {
+  test("coffeescript filter") {
+    assertUriOutputContains("/org/fusesource/scalate/scaml/coffee.scaml", """<script type='text/javascript'>
+  #<![CDATA[
+    (function() {
+      alert("Hello, Coffee!");
+    }).call(this);
+
+  #]]>
+</script>
+""")
+  }
+
+}
