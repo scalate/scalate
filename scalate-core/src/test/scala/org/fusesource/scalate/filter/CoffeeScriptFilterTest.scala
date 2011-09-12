0939c696accc0e5a93271d5d4abe01471e312521
James Strachan <james.strachan@gmail.com>
2011-09-12 07:52:26 +0100

    fix up the coffee test case

--- scalate-core/src/test/scala/org/fusesource/scalate/filter/CoffeeScriptFilterTest.scala
+++ scalate-core/src/test/scala/org/fusesource/scalate/filter/CoffeeScriptFilterTest.scala
@@ -5,12 +5,12 @@ import org.fusesource.scalate.TemplateTestSupport
 class CoffeeScriptFilterTest extends TemplateTestSupport {
   test("coffeescript filter") {
     assertUriOutputContains("/org/fusesource/scalate/scaml/coffee.scaml", """<script type='text/javascript'>
-  #<![CDATA[
+  //<![CDATA[
     (function() {
       alert("Hello, Coffee!");
     }).call(this);
 
-  #]]>
+  //]]>
 </script>
 """)
   }
