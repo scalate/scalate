77feade6e48dfdc4fcc0a1ddc37804bba00284de
Kazuhiro Sera <seratch@gmail.com>
2018-07-15 19:49:03 +0900

    Upgrade camel, jetty, karaf sheell, and snakeyaml

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   )
 
   val atmosphereJersey = "org.atmosphere" % "atmosphere-jersey" % "0.8.7"
-  val camelScala = "org.apache.camel" % "camel-scala" % "2.21.0"
+  val camelScala = "org.apache.camel" % "camel-scala" % "2.22.0"
   val camelSpring = camelScala.withName("camel-spring")
   val javaxServlet = "javax.servlet" % "servlet-api" % "2.5"
   val jaxbApi = "javax.xml.bind" % "jaxb-api" % "2.3.0"
@@ -19,7 +19,7 @@ object Dependencies {
   val jerseyServlet = "com.sun.jersey" % "jersey-servlet" % jerseyCore.revision
   val jerseyGuice = "com.sun.jersey.contribs" % "jersey-guice" % jerseyCore.revision
   val jerseyServer = "com.sun.jersey" % "jersey-server" % jerseyCore.revision
-  val jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.9.v20180320"
+  val jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.11.v20180605"
   val jettyWebapp = jettyServer.withName("jetty-webapp")
   val jettyUtil = jettyServer.withName("jetty-util")
   val jRebelSDK = "org.zeroturnaround" % "jr-sdk" % "4.6.2" from
@@ -27,7 +27,8 @@ object Dependencies {
 
   val jRubyComplete = "org.jruby" % "jruby-complete" % "9.2.0.0"
   val junit = "junit" % "junit" % "4.12"
-  val karafShell = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "4.1.5"
+  val karafShell = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "4.2.0"
+  // TODO: upgrade to 1.5.x
   val lessCssEngine = "com.asual.lesscss" % "lesscss-engine" % "1.4.2"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
   val markdownJ = "org.markdownj" % "markdownj" % "0.3.0-1.0.2b4"
@@ -48,7 +49,7 @@ object Dependencies {
   val scalaXml = (org: String) => s"$org.modules" %% "scala-xml" % "1.1.0"
   val scalaCompiler: (String, String) => ModuleID = _ % "scala-compiler" % _
   val scalaReflect: (String, String) => ModuleID = _ % "scala-reflect" % _
-  val snakeYaml = "org.yaml" % "snakeyaml" % "1.20"
+  val snakeYaml = "org.yaml" % "snakeyaml" % "1.21"
   val wikitextConfluence = "org.fusesource.wikitext" % "confluence-core" % "1.4"
   val wikitextTextile = wikitextConfluence.withName("textile-core")
 
