fe05402eeb841759d4af58636ca732f55c54c293
xuwei-k <6b656e6a69@gmail.com>
2019-06-11 05:14:06 +0000

    Update org.apache.karaf.shell.console to 4.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
 
   val jRubyComplete = "org.jruby" % "jruby-complete" % "9.2.7.0"
   val junit = "junit" % "junit" % "4.12"
-  val karafShell = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "4.2.4"
+  val karafShell = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "4.2.6"
   // TODO: upgrade to 1.5.x
   val lessCssEngine = "com.asual.lesscss" % "lesscss-engine" % "1.4.2"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
