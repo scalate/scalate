3b1bd10c8a2d32ef07838241c47e970312507cc7
xuwei-k <6b656e6a69@gmail.com>
2020-04-29 04:45:08 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
 
   val atmosphereJersey = "org.atmosphere" % "atmosphere-jersey" % "0.8.7"
-  val camelScala = "org.apache.camel" % "camel-scala" % "2.24.3"
+  val camelScala = "org.apache.camel" % "camel-scala" % "2.25.1"
   val camelSpring = camelScala.withName("camel-spring")
   val javaxServlet = "javax.servlet" % "servlet-api" % "2.5"
   val jaxbApi = "javax.xml.bind" % "jaxb-api" % "2.3.1"
@@ -14,15 +14,15 @@ object Dependencies {
   val jerseyServlet = "com.sun.jersey" % "jersey-servlet" % jerseyCore.revision
   val jerseyGuice = "com.sun.jersey.contribs" % "jersey-guice" % jerseyCore.revision
   val jerseyServer = "com.sun.jersey" % "jersey-server" % jerseyCore.revision
-  val jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.25.v20191220"
+  val jettyServer = "org.eclipse.jetty" % "jetty-server" % "9.4.28.v20200408"
   val jettyWebapp = jettyServer.withName("jetty-webapp")
   val jettyUtil = jettyServer.withName("jetty-util")
   val jRebelSDK = "org.zeroturnaround" % "jr-sdk" % "4.6.2" from
     "https://repos.zeroturnaround.com/nexus/content/groups/zt-public/org/zeroturnaround/jr-sdk/4.6.2/jr-sdk-4.6.2.jar"
 
-  val jRubyComplete = "org.jruby" % "jruby-complete" % "9.2.9.0"
+  val jRubyComplete = "org.jruby" % "jruby-complete" % "9.2.11.1"
   val junit = "junit" % "junit" % "4.12"
-  val karafShell = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "4.2.6"
+  val karafShell = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "4.2.8"
   // TODO: upgrade to 1.5.x
   val lessCssEngine = "com.asual.lesscss" % "lesscss-engine" % "1.4.2"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
@@ -36,10 +36,10 @@ object Dependencies {
   }
   val seleniumDriver = "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.52.0"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.30"
-  val springMVC = "org.springframework" % "spring-webmvc" % "5.1.12.RELEASE"
+  val springMVC = "org.springframework" % "spring-webmvc" % "5.1.15.RELEASE"
   val scalaCompiler: (String, String) => ModuleID = _ % "scala-compiler" % _
   val scalaReflect: (String, String) => ModuleID = _ % "scala-reflect" % _
-  val snakeYaml = "org.yaml" % "snakeyaml" % "1.24"
+  val snakeYaml = "org.yaml" % "snakeyaml" % "1.26"
   val wikitextConfluence = "org.fusesource.wikitext" % "confluence-core" % "1.4"
   val wikitextTextile = wikitextConfluence.withName("textile-core")
 
