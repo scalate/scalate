e71da08fdbb31e7c5e53e8b242dfe61799b81a21
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2021-06-08 17:48:28 +0000

    Update snakeyaml to 1.29

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
   val springMVC = "org.springframework" % "spring-webmvc" % "5.3.7"
   val scalaCompiler: (String, String) => ModuleID = _ % "scala-compiler" % _
   val scalaReflect: (String, String) => ModuleID = _ % "scala-reflect" % _
-  val snakeYaml = "org.yaml" % "snakeyaml" % "1.28"
+  val snakeYaml = "org.yaml" % "snakeyaml" % "1.29"
   val wikitextConfluence = "org.fusesource.wikitext" % "confluence-core" % "1.4"
   val wikitextTextile = wikitextConfluence.withName("textile-core")
 
