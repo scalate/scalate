3eb4774a0177c773b47c471b24413c1138ece62b
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-09-14 17:22:48 +0000

    Update slf4j-api to 2.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
     )
   }
   val seleniumDriver = "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.52.0"
-  val slf4jApi = "org.slf4j" % "slf4j-api" % "2.0.0"
+  val slf4jApi = "org.slf4j" % "slf4j-api" % "2.0.1"
   val springMVC = "org.springframework" % "spring-webmvc" % "5.3.22"
   val scalaCompiler: (String, String) => ModuleID = _ % "scala-compiler" % _
   val scalaReflect: (String, String) => ModuleID = _ % "scala-reflect" % _
