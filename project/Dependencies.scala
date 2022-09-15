a500d424d83673cf8e17f549ef559c1c01321564
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-09-15 09:12:09 +0000

    Update spring-webmvc to 5.3.23

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -48,7 +48,7 @@ object Dependencies {
   }
   val seleniumDriver = "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.52.0"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "2.0.1"
-  val springMVC = "org.springframework" % "spring-webmvc" % "5.3.22"
+  val springMVC = "org.springframework" % "spring-webmvc" % "5.3.23"
   val scalaCompiler: (String, String) => ModuleID = _ % "scala-compiler" % _
   val scalaReflect: (String, String) => ModuleID = _ % "scala-reflect" % _
   val snakeYaml = "org.yaml" % "snakeyaml" % "1.32"
