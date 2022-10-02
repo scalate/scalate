1ed124dd2abad1842a03df3ac8913515869df681
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-10-02 23:02:41 +0000

    Update logback-classic to 1.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
   val jRubyComplete = "org.jruby" % "jruby-complete" % "9.3.8.0"
   val junit = "junit" % "junit" % "4.13.2"
   val karafShell = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "4.4.1"
-  val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.3.2"
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.3.3"
   val osgiCore = "org.osgi" % "org.osgi.core" % "6.0.0"
   val rhinoCoffeeScript = "tv.cntt" % "rhinocoffeescript" % "1.12.7"
   val scalamd = "org.scalatra.scalate" %% "scalamd" % "1.7.3"
