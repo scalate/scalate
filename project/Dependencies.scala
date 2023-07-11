46d0f0b3a6a598849c7d2ec6ceb70a5d3dc9fa04
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2023-07-11 00:08:43 +0000

    Update scala-xml to 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
       case Some((2, 11)) =>
         "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
       case _ =>
-        "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
+        "org.scala-lang.modules" %% "scala-xml" % "2.2.0"
     }
   )
 
