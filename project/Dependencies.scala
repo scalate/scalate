b9c8e839fcd6557315b09ed564ebf4c6139a781a
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2024-06-23 02:05:45 +0000

    Update scalatest-funsuite to 3.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   val scalamd = ("org.scalatra.scalate" %% "scalamd" % "1.8.0")
   val scalaTest = Def.setting {
     Seq(
-      "org.scalatest" %% "scalatest-funsuite" % "3.2.18",
+      "org.scalatest" %% "scalatest-funsuite" % "3.2.19",
       "org.scalatest" %% "scalatest-shouldmatchers" % "3.2.18",
       "org.scalatestplus" %% "junit-4-13" % "3.2.18.0",
     )
